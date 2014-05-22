5aaaac6b5c55df52183f49188b234d7073a31a65
Thomas Maurer <0876337@hr.nl>
2014-05-22 14:30:07 +0200

    Search activty gemaakt

--- app/src/main/java/example/myapplication/movelife/Menu.java
+++ app/src/main/java/example/myapplication/movelife/Menu.java
@@ -1,11 +1,15 @@
 package example.myapplication.movelife;
 
 import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 /**
  * Created by Thomas on 15-5-2014.
@@ -18,7 +22,13 @@ public class Menu extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
+
+
+
+
     }
+
+
 
 
     @Override
@@ -40,7 +50,18 @@ public class Menu extends ListActivity {
 
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
         return true;
     }
+
+
 
 }
